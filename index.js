1079bb85345a97a6b9c4374ca410c6856fd88183
marcus featherston <marcusovfeatherston@gmail.com>
2023-02-05 10:59:07 -0700

    Set up event listener for the DOMContent loaded

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+  const replaceText = document.querySelector("#text");
+  replaceText.textContent = "This is really cool!";
+});
